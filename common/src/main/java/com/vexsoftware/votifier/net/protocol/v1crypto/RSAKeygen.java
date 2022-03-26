5b8b8baca478e6f0a19ce3319a8b161f15b32dcf
siloneco <siloneco4646@gmail.com>
2022-03-26 15:58:29 +0900

    Apply google-java-format

--- common/src/main/java/com/vexsoftware/votifier/net/protocol/v1crypto/RSAKeygen.java
+++ common/src/main/java/com/vexsoftware/votifier/net/protocol/v1crypto/RSAKeygen.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2011 Vex Software LLC
  * This file is part of Votifier.
- * 
+ *
  * Votifier is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Votifier is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Votifier.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,16 +28,17 @@ import java.security.spec.RSAKeyGenParameterSpec;
  * @author Blake Beaupain
  */
 public class RSAKeygen {
-    /**
-     * Generates an RSA key pair.
-     *
-     * @param bits The amount of bits
-     * @return The key pair
-     */
-    public static KeyPair generate(int bits) throws Exception {
-        KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
-        RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(bits, RSAKeyGenParameterSpec.F4);
-        keygen.initialize(spec);
-        return keygen.generateKeyPair();
-    }
+
+  /**
+   * Generates an RSA key pair.
+   *
+   * @param bits The amount of bits
+   * @return The key pair
+   */
+  public static KeyPair generate(int bits) throws Exception {
+    KeyPairGenerator keygen = KeyPairGenerator.getInstance("RSA");
+    RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(bits, RSAKeyGenParameterSpec.F4);
+    keygen.initialize(spec);
+    return keygen.generateKeyPair();
+  }
 }
