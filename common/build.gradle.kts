390e36c69ba67421a3bfc0572da4226087d390d1
acrylic-style <me@acrylicstyle.xyz>
2022-03-26 14:13:04 +0900

    Add common files

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -3,6 +3,8 @@ repositories {
 }
 
 dependencies {
+    // gson
+    api("com.google.code.gson:gson:2.8.5")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
