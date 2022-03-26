b3d85c91b101ce36c78fe811363e92ae87cb8208
acrylic-style <me@acrylicstyle.xyz>
2022-03-26 14:14:37 +0900

    Add annotations dependency

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -5,6 +5,7 @@ repositories {
 dependencies {
     // gson
     api("com.google.code.gson:gson:2.8.5")
+    compileOnlyApi("org.jetbrains:annotations:23.0.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
