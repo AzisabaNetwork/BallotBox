ee426a6ce412b59157348195eba1aaa10e33b887
acrylic-style <me@acrylicstyle.xyz>
2023-01-11 14:26:28 +0900

    deploy to repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,12 @@
 plugins {
     java
     `java-library`
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+    `maven-publish`
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-group = "net.azisaba"
+group = "net.azisaba.ballotbox"
 version = "1.0.1"
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
-}
 
 subprojects {
     group = parent!!.version
@@ -18,16 +15,52 @@ subprojects {
     apply {
         plugin("java")
         plugin("java-library")
+        plugin("maven-publish")
         plugin("com.github.johnrengelman.shadow")
-    }
-
-    java {
-        toolchain.languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
 
-tasks {
-    test {
-        useJUnitPlatform()
+allprojects {
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    val javaComponent = components["java"] as AdhocComponentWithVariants
+    javaComponent.withVariantsFromConfiguration(configurations["sourcesElements"]) {
+        skip()
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = "repo"
+                credentials(PasswordCredentials::class)
+                url = uri(
+                    if (project.version.toString().endsWith("SNAPSHOT"))
+                        project.findProperty("deploySnapshotURL") ?: System.getProperty("deploySnapshotURL", "https://repo.azisaba.net/repository/maven-snapshots/")
+                    else
+                        project.findProperty("deployReleasesURL") ?: System.getProperty("deployReleasesURL", "https://repo.azisaba.net/repository/maven-releases/")
+                )
+            }
+        }
+
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"])
+                artifact(tasks.getByName("sourcesJar"))
+            }
+        }
+    }
+
+    tasks {
+        compileJava {
+            options.encoding = "UTF-8"
+        }
+
+        test {
+            useJUnitPlatform()
+        }
     }
 }
